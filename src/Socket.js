25bb1431d0cb60be3d0a4120f1a1adec0add730b
Bharath Rao <bharath.rao@gmail.com>
2017-02-02 16:56:56 -0800

    Ignore invalid/obsolete auth failures

--- src/Socket.js
+++ src/Socket.js
@@ -9,6 +9,10 @@ module.exports = function (baseUrl, account, errorHandler) {
   var socket     = io(baseUrl, { rejectUnauthorized: true });
   socket.logging = false
 
+  affirm(account.userid, 'Missing userid in account')
+  affirm(account.secret, 'Missing secret in account')
+  affirm(account.userPublicKey, 'Missing userPublicKey in account')
+
   socket.send = function (request) {
     var method  = request.method
     var uri     = request.uri
@@ -33,10 +37,12 @@ module.exports = function (baseUrl, account, errorHandler) {
   }
 
   socket.onAuthError = function (message) {
+    if(!validMessage(message)) return console.log('*** WARNING: Ignoring invalid server message: ', message)
     if (message.data.retry) return errorHandler && errorHandler(message.error)
     nonce.calibrate(Date.now(), message['server-time'])
     var auth = message.data.headers.Authorization
     var data = cache.get(auth)
+    if(!data) return console.log('*** WARNING: Skipping retry for invalid Authorization', auth)
     socket.send({ method: data.method, uri: data.uri, headers: data.headers, body: data.body, params: data.params, retry: true })
   }
 
@@ -51,6 +57,10 @@ module.exports = function (baseUrl, account, errorHandler) {
   socket.on('server-time', function (serverTime) {
     nonce.calibrate(Date.now(), serverTime)
   })
+
+  function validMessage(message) {
+    return message && message.data && message.data.headers && message.data.headers.Authorization
+  }
 
   return socket
 }
